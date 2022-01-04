24d3ab5f9c345c8c4e75f39076981820a88f10c7
Leong Teng Man <41246170+taimoon@users.noreply.github.com>
2022-01-05 07:17:27 +0800

    Implement insertin freight

--- mysqlusingpython.py
+++ mysqlusingpython.py
@@ -1,23 +1,42 @@
 from mysql.connector import (connection)
-cnx = connection.MySQLConnection(user='root', password='20019', host='127.0.0.1', database='portmanagementdb')
-cursor = cnx.cursor()
+import datetime
+import random
 def getInFreight(cursor):
     query = ("SELECT listID, freightID FROM list "
              "WHERE freightDirection = TRUE;")
-    cursor.execute(query)
     resList = []
+    cursor.execute(query)
     for i in cursor:
         resList.append(i)
     return resList
+def isFreightHere(cursor, freightID):
+    query = (f"select freightID from list where freightID = '{freightID}';")
+    i = cursor.execute(query)
+    return i == None
 def insertInList(cursor, instance):
+    xLim = 10
+    yLim = 10
+    stackMax = 10
+    if instance['freightDirection'] == False or isFreightHere(cursor, instance['freightID']) != True :
+        return
     addFreightQuery = ("INSERT INTO list(listID, freightID, freightTypeID, targetID, freightDirection, origin) "
                        "VALUES (%(listID)s, %(freightID)s, %(freightTypeID)s,"
                        "%(targetID)s, %(freightDirection)s, %(origin)s);"
                        )
     cursor.execute(addFreightQuery, instance)
+    addFreightQuery = ("INSERT INTO freight(inListID, freightID, arrivalTime, areaID,locX, locY, stackLvl) "
+                       "VALUES (%s, %s, now(), %s,%s, %s, %s);")
+    data = (instance['listID'], instance['freightID'], random.choice('ABCD'),
+            random.randint(0,xLim),random.randint(0,yLim), random.randint(0,stackMax))
+    cursor.execute(addFreightQuery, data)
+
+
 def deleteFreight(cursor, freightID):
     dropFreight = (f"DELETE FROM list WHERE freightID='{freightID}';")
     cursor.execute(dropFreight)
+
+cnx = connection.MySQLConnection(user='root', password='20019', host='127.0.0.1', database='portmanagementdb')
+cursor = cnx.cursor(buffered=True)
 instance = {
     'listID': "1",
     'freightID': "CMSU7773080",
@@ -26,7 +45,11 @@ instance = {
     'freightDirection':True,
     'origin':"Lunarian"
     }
-#insertInList(cursor, instance)
-deleteFreight(cursor, "CMSU7773080")
+
+#deleteFreight(cursor, "CMSU7773080")
+insertInList(cursor, instance)
+print(getInFreight(cursor))
+
+
 #cnx.commit()
 cnx.close()
