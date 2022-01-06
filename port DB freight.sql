c825932dc6cc10e739c1edfce9237ed096e15ec7
Leong Teng Man <41246170+taimoon@users.noreply.github.com>
2022-01-06 14:00:20 +0800

    Improving Random Generator

---
+++ port DB freight.sql
@@ -0,0 +1,12 @@
+INSERT INTO list(freightID, listID, freightTypeID, targetID, freightDirection, origin) VALUES 
+("GSKU5033562",1, 20G0, "GSK", FALSE, "Philippines"),
+("GSKU9235129",1, 20G0, "GSK", TRUE, "United Kingdom"),
+("GSKU2415807",1, 20G0, "CMS", TRUE, "France"),
+("GSKU5586505",1, 20G0, "GSK", FALSE, "Thailand"),
+("GSKU3261223",1, 20G0, "CMS", TRUE, "Philippines"),
+("GSKU3230259",1, 20G0, "CMS", FALSE, "South Africa"),
+("GSKU2810925",1, 20G0, "CMS", FALSE, "France"),
+("GSKU9744960",1, 20G0, "GSK", FALSE, "Europe"),
+("GSKU8015583",1, 20G0, "CMS", FALSE, "France"),
+("GSKU0931569",1, 20G0, "CMS", FALSE, "Japan"),
+("GSKU8954386",1, 20G0, "CMS", TRUE, "United States");
